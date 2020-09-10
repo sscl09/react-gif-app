7ba06d665392fbb4472db951a9c5036c8398ab29
SCL <sofiasscl@gmail.com>
2020-09-09 21:31:30 -0500

    docs agregado

---
+++ docs/precache-manifest.d46bb39ba6b5c4e97ec40dff0143d90f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8cece2ca2f7d539aee434fee97af46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89626b1a0a7206ebf2ce",
+    "url": "/static/css/main.5b30875c.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89626b1a0a7206ebf2ce",
+    "url": "/static/js/main.ef50ed5b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
